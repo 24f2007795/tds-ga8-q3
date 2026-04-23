eb12ea1a679415c0100e652b729b8b3dad62ada7
24f2007795 <24f2007795@ds.study.iitm.ac.in>
2026-04-23 12:14:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+app = FastAPI()
+
+# Load dataset & train model at startup
+iris = load_iris()
+X, y = iris.data, iris.target
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+class_names = iris.target_names
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.get("/predict")
+def predict(sl: float, sw: float, pl: float, pw: float):
+    sample = [[sl, sw, pl, pw]]
+    pred = model.predict(sample)[0]
+    return {
+        "prediction": int(pred),
+        "class_name": class_names[pred]
+    }
